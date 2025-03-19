0dd84327a060b1a405b3d501521cee2bc7574ba2
Intisar-Ahmad <intisar12122000@gmail.com>
2025-03-19 14:14:39 +0500

    Redux

--- redux/src/components/Navbar.jsx
+++ redux/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux'
 function Navbar() {
+  const count = useSelector((state) => state.counter.value)
   return (
     <div>
-      this is a navabar
+      this is a navabar{count}
     </div>
   )
 }
